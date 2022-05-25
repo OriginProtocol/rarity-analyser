d73a918e707be1832c39548c42ed334a978bba9d
shahthepro <shahthepro@outlook.com>
2022-05-25 16:30:51 +0530

    Bump to 1.0.3

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { analyse } from './src/analysis';
-export default analyse;
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const analysis_1 = require("./src/analysis");
-exports.default = analysis_1.analyse;
-//# sourceMappingURL=index.js.map
